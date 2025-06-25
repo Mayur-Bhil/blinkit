93cdd9aad8487587529c8065b7f030c35c60465f
Mayur Bhil <mayurbhil47@gmail.com>
2025-06-25 17:49:30 +0530

    cartProduct model add

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -10,5 +10,42 @@ const orderSchema = new mongoose.Schema({
         required:[true,"Provide orderId"],
         unique:true
     },
+    productId:{
+        type:mongoose.Schema.ObjectId,
+        ref:"Product"
+    },
+    product_details:{
+        name:String,
+        image:Array,
+    },
+    paymentId:{
+        type:String,
+        default:""
+    },
+    payment_status:{
+        type:String,
+        default:""
+    },
+    delivery_address:{
+        type :mongoose.Schema.ObjectId,
+        ref:"Address"
+    },
+    subTotalAmount:{
+        type :Number,
+        default:0
+    },
+    totalAmount:{
+        type:Number,
+        default:0,
+    },
+    invoice_receipt:{
+        type:String,
+        default: ""
+    }
     
-})
+},{
+    timestamps:true
+});
+
+const Order = mongoose.model("Order",orderSchema)
+export default Order;
