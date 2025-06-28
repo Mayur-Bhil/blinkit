e5e5a0595c5fc0105064f0d0b48c08115aa5652d
Mayur Bhil <mayurbhil47@gmail.com>
2025-06-28 15:47:15 +0530

    login controller add

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,6 +2,8 @@ import sendEmail from "../config/sendEmail.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
 import verificatioEmailTemplate from "../utils/verifyEmailTEmplate.js";
+import generateAccessToken from "../utils/generateAccessToken.js";
+import generateRefreshToken from "../utils/generateRefressToken.js";
 
 export default async function registerUserController(req,res) {
     try {
@@ -93,5 +95,69 @@ export async function verifyEmailController(req,res){
             error:true,
             success:true
         })    
+    }
+}
+
+//login controller
+
+export async function userLoginController(req,res){
+    try {
+        const {email,password} = req.body;
+        if(!email || !password){
+            res.status(400).josn({
+                message:"please provide email and password",
+                error:true, 
+                success:false
+            })
+        }
+        const user = await User.findOne({email:email});
+
+        if(!user){
+            res.status(400).json({
+                message: "user not registered",
+                error:true,
+                success:false
+            })
+        }
+        if(user.status !== "active"){
+            return res.status(400).json({
+                message: "contact to admin",
+                error:true,
+                success:false
+            })
+        }
+
+        const checkPassword = await bcrypt.compare(password,user.password);
+        if(!checkPassword){
+            return res.status(400).josn({
+                message:"check your password",
+                error:true,
+                success:false
+            })
+        }
+        const accessToken = await generateAccessToken(user._id);
+        const refreshToken = await generateRefreshToken(user._id);
+        const cookieOptions = {
+            httponly:true,  
+            secure:true,
+            sameSite:"None"
+        }
+        res.cookie('accessToken',accessToken,cookieOptions);
+        res.cookie('refressToken',refreshToken,cookieOptions);
+        return res.send({
+            message :"Login SuccessFully",
+            error:false,
+            success:true,
+            data : {
+                accessToken,
+                refreshToken
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:error.message || error,
+            error:true,
+            success:false
+        })
     }
 }
